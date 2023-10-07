0c5708db36c5baa375389d74f12005bf7e7277b5
Christian <christianperez.rbb@gmail.com>
2023-10-07 19:53:39 +0200

    feat: I do button delete and accept in javaScript to page game.

--- js/game.js
+++ js/game.js
@@ -22,3 +22,46 @@ const coloresSeleccionados = document.querySelectorAll(".color-seleccionado");
 coloresSeleccionados.forEach((spanColor, index) => {
   spanColor.style.backgroundColor = colores[index];
 });
+const botones = document.querySelectorAll(".color-seleccionado");
+let elementosFilaActual = document.querySelectorAll(`.fila-${filaActual}`);
+const elementosPintados = new Set();
+let indiceElementoActual = 0;
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const colorSeleccionado = boton.style.backgroundColor;
+
+    if (elementosPintados.has(elementosFilaActual[indiceElementoActual])) {
+      return;
+    }
+
+    elementosFilaActual[indiceElementoActual].style.backgroundColor = colorSeleccionado;
+    elementosPintados.add(elementosFilaActual[indiceElementoActual]);
+
+    indiceElementoActual++;
+
+    if (indiceElementoActual === elementosFilaActual.length) {
+    }
+  });
+});
+
+const botonBorrar = document.querySelector(".borrar");
+
+botonBorrar.addEventListener("click", () => {
+  if (elementosPintados.size > 0) {
+    const ultimoElementoPintado = elementosFilaActual[indiceElementoActual - 1];
+    ultimoElementoPintado.style.backgroundColor = "";
+
+    elementosPintados.delete(ultimoElementoPintado);
+    indiceElementoActual--;
+  }
+});
+
+document.querySelector("#aceptar1").addEventListener("click", () => {
+  if (indiceElementoActual === elementosFilaActual.length) {
+    compararCombinacion();
+    bloquearFilaActual();
+    pasarSiguienteFila();
+  } else {
+  }
+});
